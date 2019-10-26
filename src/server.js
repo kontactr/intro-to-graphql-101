e664ef5512a413adb2c138ff31214332b0d658d7
Yash Gandhi <gandhiyash1381997@gmail.com>
2019-10-27 00:50:30 +0530

    base setup

--- src/server.js
+++ src/server.js
@@ -1,33 +1,43 @@
-import { ApolloServer } from 'apollo-server'
-import { loadTypeSchema } from './utils/schema'
-import { merge } from 'lodash'
-import config from './config'
-import { connect } from './db'
-import product from './types/product/product.resolvers'
-import coupon from './types/coupon/coupon.resolvers'
-import user from './types/user/user.resolvers'
+import { ApolloServer } from "apollo-server";
+import { loadTypeSchema } from "./utils/schema";
+import { merge } from "lodash";
+import config from "./config";
+import { connect } from "./db";
+import product from "./types/product/product.resolvers";
+import coupon from "./types/coupon/coupon.resolvers";
+import user from "./types/user/user.resolvers";
 
-const types = ['product', 'coupon', 'user']
+const types = ["product", "coupon", "user"];
 
 export const start = async () => {
   const rootSchema = `
+    type Query{
+      hello: String
+    }
+
     schema {
       query: Query
     }
-  `
-  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
+  `;
+  const schemaTypes = await Promise.all(types.map(loadTypeSchema));
 
   const server = new ApolloServer({
     typeDefs: [rootSchema],
-    resolvers: {},
+    resolvers: {
+      Query: {
+        hello: () => {
+          return "Hello World";
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
-      return { user: null }
+      return { user: null };
     }
-  })
+  });
 
-  await connect(config.dbUrl)
-  const { url } = await server.listen({ port: config.port })
+  await connect(config.dbUrl);
+  const { url } = await server.listen({ port: config.port });
 
-  console.log(`GQL server ready at ${url}`)
-}
+  console.log(`GQL server ready at ${url}`);
+};
